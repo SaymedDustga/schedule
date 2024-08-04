6dd127a64ae6f67979835625566467bf4249bc29
Saymed <gasayagom@gmail.com>
2024-08-03 23:13:14 -0400

    first step

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
-/*$(document).ready(function() {
-    $.getJSON("horarios.json", function(data) {
-        $.each(data, function(day, schedule) {
-            $.each(schedule, function(time, available) {
-                var checkbox = $("table#schedule td:contains('" + day + "')").siblings().first(); // Encuentra la celda correspondiente al día y selecciona el primer checkbox
-
-                if (available) {
-                    checkbox.find("input[type='checkbox']").prop("checked", true); // Marca el checkbox si el horario está disponible
-                } else {
-                    checkbox.find("input[type='checkbox']").prop("checked", false); // Desmarca el checkbox si el horario no está disponible
-                }
-            });
-        });
-    });
-});*/
+const mod_button = document.getElementById("modificar");
+const save_button = document.getElementById("guardar");
 
 document.addEventListener("DOMContentLoaded", async function() {
     try {
-        // const response = await fetch("C:\\Users\\Saymed Dustga\\Desktop\\Semestre B2024\\Ingenieria del Software\\Development\\schedule.json");
-        // const response = await fetch("C:/Users/Saymed Dustga/Desktop/Semestre B2024/Ingenieria del Software/Development/schedule.json");
-        const response = await fetch("schedule.json");
+        const response = await fetch("https://saymeddustga.github.io/schedule/schedule.json");
         const data = await response.json();
-
-        console.log("test");
 
         Object.entries(data).forEach(([day, schedule]) => {
             Object.entries(schedule).forEach(([time, available]) => {
-                const checkbox = document.querySelector(`table#schedule td:contains('${day}')`).nextElementSibling.querySelector("input[type='checkbox']");
+                const noSpacesTime = time.replace(/\s/g, "").replace(/:/g, "");
+                const selectorString = `table tr > td#${day}${noSpacesTime.toLowerCase()} > input[type="checkbox"]`;
+                const checkbox = document.querySelector(selectorString);
+
                 if (available) {
                     checkbox.checked = true;
+                    const parent = checkbox.parentNode;
+                    parent.style.background = "green";
+                    const label = this.createElement("label");
+                    label.textContent = "Disponible";
+                    parent.appendChild(label);
+
                 } else {
                     checkbox.checked = false;
+                    const parent = checkbox.parentNode;
+                    parent.style.background = "red";
+                    const label = this.createElement("label");
+                    label.textContent = "Disponible";
+                    parent.appendChild(label);
                 }
+                checkbox.style.visibility = "hidden";
+                checkbox.addEventListener("change", function() {
+                    if (this.checked) {
+                        this.parentNode.style.background = "green";
+                    } else {
+                        this.parentNode.style.background = "red";
+                    }
+                });
             });
         });
     } catch (error) {
         console.error("Error fetching or parsing JSON file:", error);
     }
+});
+
+mod_button.addEventListener("click", function() {
+    const checkboxs = document.querySelectorAll('table input[type="checkbox"]');
+    checkboxs.forEach((element) => {
+        element.style.visibility = "visible";
+    })
+    save_button.disabled = false;
+    mod_button.disabled = true;
+});
+
+save_button.addEventListener("click", function() {
+    const checkboxs = document.querySelectorAll('table input[type="checkbox"]');
+    checkboxs.forEach((element) => {
+        element.style.visibility = "hidden";
+    })
+    save_button.disabled = true;
+    mod_button.disabled = false;
 });
